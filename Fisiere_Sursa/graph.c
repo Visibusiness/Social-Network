86db7639573f385c6773879460c09c3fbf9e8372
Visi <visanescu.business@gmail.com>
2024-05-17 20:17:21 +0300

    grapphuri

--- Fisiere_Sursa/graph.c
+++ Fisiere_Sursa/graph.c
@@ -1,15 +1,81 @@
 #include "graph.h"
 
-graph *new_graph() 
+node_t *new_node(void *data)
 {
-    graph *x = malloc(sizeof(graph));
-    x->max_size = 0;
+    node_t *x = malloc(sizeof(node_t));
+    x->data = data;
+    x->next = NULL;
+    x->prev = NULL;
+    return x;
+}
+
+void link(node_t *x, node_t *y)
+{
+    if(y)
+        x->next = y;
+    if(x)
+        y->prev = x;    
+}
+
+list_t *new_list(unsigned int data_size)
+{
+    list_t *x = malloc(sizeof(list_t));
+    x->head = NULL;
+    x->size = 0;
+    x->data_size = data_size;
+    return x;
+}
+
+void add_in_list(list_t *list, node_t* node)
+{
+    link(node, list->head);
+    list->head = node;
+    list->size++;
+}
+
+node_t *remove_from_list(list_t *list , void *data, int (*compare_function)(void *, void *))
+{
+    node_t *cr = list->head;
+    while(cr){
+        if(!compare_function(cr->data, data)) {
+            if(cr != list->head)
+                link(cr->prev, cr->next);
+            else {
+                list->head = cr->next;
+                list->head->prev = NULL;
+            }
+            list->size--;
+            return cr;
+        }
+        cr = cr->next;
+    }
+    return NULL;
+}
+
+void ll_free(list_t **pp_list, void (*free_function)(void *))
+{
+    if (!pp_list || !*pp_list)
+        return;
+    node_t *cr = (*pp_list)->head, *next;
+    while(cr) {
+        next = cr->next;
+        free_function(cr->data);
+        free(cr);
+        cr = next;
+    }
+    free(*pp_list);
+    *pp_list = NULL;
+}
+
+graph_t *new_graph() 
+{
+    graph_t *x = malloc(sizeof(graph_t));
     x->nodes = 0;
     x->friends = NULL;
     return x;
 }
 
-void add_node(graph *x, unsigned int node)
+void add_node(graph_t *x, unsigned int node)
 {
     if(x->max_size == 0)
         x->max_size = 1;
@@ -18,7 +84,7 @@ void add_node(graph *x, unsigned int node)
     x->friends[node] = new_list(sizeof(int));
 }
 
-void add_connection(graph *x, unsigned int a, unsigned int b)
+void add_connection(graph_t *x, unsigned int a, unsigned int b)
 {
     int *u = malloc(sizeof(int)); 
     int *v = malloc(sizeof(int));
@@ -28,7 +94,7 @@ void add_connection(graph *x, unsigned int a, unsigned int b)
     add_node(x->friends[b], new_node(v));
 }
 
-void remove_connection(graph *x, unsigned int a, unsigned int b)
+void remove_connection(graph_t *x, unsigned int a, unsigned int b)
 {
 
     remove_node(x->friends[a], &b);
@@ -36,4 +102,4 @@ void remove_connection(graph *x, unsigned int a, unsigned int b)
 
 }
 
-void get_distance(graph *x, unsigned int a, unsigned int b);
+void get_distance(graph_t *x, unsigned int a, unsigned int b);
--- Fisiere_Sursa/graph.h
+++ Fisiere_Sursa/graph.h
@@ -1,16 +1,42 @@
-typedef struct graph graph;
+typedef struct graph_t graph_t;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "hashtable.h"
+#define MAX_STRING_SIZE 256
+#define HMAX 10
 
-struct graph
+typedef struct node_t node_t;
+
+struct node_t
+{
+    void *data;
+    node_t *prev, *next;
+};
+
+typedef struct list_t
+{
+    node_t *head;
+    unsigned int data_size;
+    unsigned int size;
+} list_t;
+
+node_t *new_node(void *data);
+node_t *remove_from_list(list_t *list , void *data);
+list_t *new_list();
+void add_in_list(list_t *list, node_t* node);
+void link(node_t *x, node_t *y);
+list_t *ll_create(unsigned int data_size);
+void ll_free(list_t **pp_list);
+
+struct graph_t
 {
     unsigned int nodes;
-    unsigned int max_size;
     list_t **friends;
 };
 
-graph *new_graph();
-void add_node(graph *x, unsigned int node);
-void add_connection(graph *x, unsigned int a, unsigned int b);
-void remove_connection(graph *x, unsigned int a, unsigned int b);
-void get_distance(graph *x, unsigned int a, unsigned int b);
+graph_t *new_graph();
+void add_node(graph_t *x, unsigned int node);
+void add_connection(graph_t *x, unsigned int a, unsigned int b);
+void remove_connection(graph_t *x, unsigned int a, unsigned int b);
+void get_distance(graph_t *x, unsigned int a, unsigned int b);
