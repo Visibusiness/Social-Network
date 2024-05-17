2ca4d6227ef5ebb42dbc73fa32e7fb33a4275141
Visi <visanescu.business@gmail.com>
2024-05-17 18:49:52 +0300

    graph.c

--- Fisiere_Sursa/graph.c
+++ Fisiere_Sursa/graph.c
@@ -1,3 +1,35 @@
 #include "graph.h"
 
+graph *new_graph() 
+{
+    graph *x = malloc(sizeof(graph));
+    x->max_size = 0;
+    x->nodes = 0;
+    x->friends = NULL
+    return x;
+}
 
+void add_node(graph *x, unsigned int node)
+{
+    if(x->max_size == 0)
+        x->max_size = 1;
+    if(x->nodes + 1 > x->max_size)
+        x->max_size <<= 1;
+    x->friends[node] = new_list();
+}
+
+void add_connection(graph *x, unsigned int a, unsigned int b)
+{
+    add_node(x->friends[a], new_node(&b, sizeof(int)));
+    add_node(x->friends[b], new_node(&a, sizeof(int)));
+}
+
+void remove_connection(graph *x, unsigned int a, unsigned int b)
+{
+
+    remove_node(x->friends[a], &b);
+    remove_node(x->friends[b], &a);
+
+}
+
+void get_distance(graph *x, unsigned int a, unsigned int b);
